8da70818de290ad5bad5f97f1063ee8dfbe46fbd
terrtup <427965559@qq.com>
2023-05-20 01:38:54 -0700

    增加内存池模块

--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -27,11 +27,6 @@ if(NOT CMAKE_INSTALL_COMPONENT)
   endif()
 endif()
 
-# Install shared libraries without execute permission?
-if(NOT DEFINED CMAKE_INSTALL_SO_NO_EXE)
-  set(CMAKE_INSTALL_SO_NO_EXE "1")
-endif()
-
 # Is this installation the result of a crosscompile?
 if(NOT DEFINED CMAKE_CROSSCOMPILING)
   set(CMAKE_CROSSCOMPILING "FALSE")
